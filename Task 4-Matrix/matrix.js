e5b2da65016026b26880cc5b5114bceee97d9180
evankimani <kimanievan73@gmail.com>
2024-04-27 12:25:39 +0300

    new

--- Task 4-Matrix/matrix.js
+++ Task 4-Matrix/matrix.js
@@ -3,45 +3,35 @@
 
         btn.addEventListener('click', () => {
             const rows = parseInt(document.getElementById("rows").value);
+            console.log(rows)
             const columns = parseInt(document.getElementById("columns").value);
             const matrix = [];
-
-            // Generate new matrix
-            for (let i = 0; i < rows; i++) {
-                matrix.push([]);
-                for (let j = 0; j < columns; j++) {
-                    matrix[i].push(Math.floor(Math.random() * 100)); 
-                }
-            }
-
-            // Display matrix in the table
             const table = document.getElementById("matrixTable");
             table.innerHTML = '';
-
+            // Generate new matrix
+            if(rows>columns){
+                alert('columns should be greater or equal to rows')
+            }
             for (let i = 0; i < rows; i++) {
+                matrix.push([]);
                 let row = table.insertRow();
                 for (let j = 0; j < columns; j++) {
+                    matrix[i].push(Math.floor(Math.random() * 100)); 
                     let cell = row.insertCell();
                     cell.textContent = matrix[i][j];
                 }
             }
-            //slice the matrx in the shaded region
-            let sliced = matrix[0].slice(rows-1);
            
-            for (let i = 1; i < matrix.length; i++) {
-           
-                sliced = sliced.concat(matrix[i].slice(rows-1-i, matrix[i].length- i));
-                }
-               
-            
             //obtain the largest element in the shaded region
-            let largest = sliced[0]
-            for (let i = 0; i < sliced.length; i++) {
-                if (sliced[i] > largest) {
-                    largest = sliced[i]
+            let largest = matrix[0][rows-1]
+            for (let i = 0; i < rows; i++) {
+                for(let j=rows-1-i; j<=columns-i;j++){
+                    if (matrix[i][j] > largest) {
+                        largest = matrix[i][j]
                 }
+                }   
             }
-        
              document.getElementById('large').value = largest
         })
-    
+    const str='john'
+    console.log(str.includes('m'))
