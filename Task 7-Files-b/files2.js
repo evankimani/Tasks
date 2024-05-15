26fe58d2555890b45a7666679ba53fb2e0b66825
evankimani <kimanievan73@gmail.com>
2024-05-15 16:08:55 +0300

    task 7b

---
+++ Task 7-Files-b/files2.js
@@ -0,0 +1,32 @@
+function removeDuplicatesInSequences(sequences) {
+    const processedSequences = sequences.map(sequence => {
+        const numbers = sequence.split('').map(Number);
+         const countMap = numbers.reduce((map, num) => {
+            map[num] = (map[num] || 0) + 1;
+            return map;
+        }, {});
+        const uniqueNumbers = numbers.filter(num => countMap[num] === 1);
+        return uniqueNumbers.join('');
+    });
+ 
+    return processedSequences;
+}
+const inputFile=document.getElementById('fileInput')
+
+inputFile.addEventListener('change',(e)=>{
+    const file=e.target.files[0]
+    const reader=new FileReader()
+reader.addEventListener('load',(e)=>{
+    const fileContent= e.target.result
+//console.log(fileContent)
+     const sequenceArr=fileContent.split('0')
+     const modifiedFile=removeDuplicatesInSequences(sequenceArr).join('0')
+     const modifiedFileSeqence=modifiedFile.split('').join(',')
+     const blob=new Blob([modifiedFileSeqence],{type:'text/plain'})
+link.href=URL.createObjectURL(blob)
+    
+    })
+    reader.readAsText(file)
+})
+
+
